06244903b1be07a4fbfe4a4aef96ad60fe01cf69
maschang <kyohei.masuda@icloud.com>
2026-03-06 10:44:00 +0900

    nvim ステータスバーにpathも表示する

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -105,7 +105,7 @@ return {
         sections = {
           lualine_a = { "mode" },
           lualine_b = { "branch" },
-          lualine_c = { "filename" },
+          lualine_c = { { "filename", path = 1 } },
           lualine_x = { git_blame },
           lualine_y = { "filetype" },
           lualine_z = { "location" },
